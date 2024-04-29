51860a359f2d1a8c108517ebc57f6a4a2f76cb42
Jamiss <113078836+jimsjamiss@users.noreply.github.com>
2024-04-29 20:46:56 +0800

    updated - james

---
+++ TropicTrail/TropicTrail/Content/js/popup-box.js
@@ -0,0 +1,14 @@
+﻿let popup = document.getElementById("pop-up");
+let username = document.getElementById("userName");
+let password = document.getElementById("password");
+let confirmPass = document.getElementById("confirmPass");
+let email = document.getElementById("email");
+
+if (username != null && password != null && confirmPass != null && email != null) {
+    function openPopup() {
+        popup.classList.add("open-popup");
+    }
+    function closePopup() {
+        popup.classList.remove("open-popup");
+    }
+}
